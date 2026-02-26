fac8db2a9574a5c38fd9513547d035c750499138
Gottfreud <github@sandalow.net>
2026-02-26 16:48:49 +0100

    Знакомство с API клиентами. Практика: Написание API клиента

---
+++ clients/users/public_users_client.py
@@ -0,0 +1,29 @@
+from typing import TypedDict
+from httpx import Response
+from clients.api_client import APIClient
+
+class PublicUserDict(TypedDict):
+    """
+       Описание структуры данных пользователя
+    """
+
+    email: str
+    password: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+class PublicUsersClient(APIClient):
+    """
+    Клиент для работы с /api/v1/users
+    """
+
+    def create_user_api(self, request: PublicUserDict) -> Response:
+        """
+            Метод создает пользователя
+
+            :param request: Словарь с данными пользователя
+            :return: Ответ от сервера в виде объекта httpx.Response
+        """
+
+        return self.post("/api/v1/users", json=request)
