058fdfb1c95bb7f54e5366e93192ad0a18820409
Gottfreud <github@sandalow.net>
2026-02-28 20:06:44 +0100

    Знакомство с Pydantic. Практика написания Pydantic-моделей

--- pydantic_create_user.py
+++ pydantic_create_user.py
@@ -3,21 +3,21 @@ from pydantic import BaseModel, Field, EmailStr
 class UserSchema(BaseModel):
     id: str
     email: EmailStr
-    last_name: Field(alias="lastName")
-    first_name: Field(alias="firstName")
-    middle_name: Field(alias="middleName")
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
 
 class CreateUserRequestSchema(BaseModel):
     email: EmailStr
     password: str
-    last_name: Field(alias="lastName")
-    first_name: Field(alias="firstName")
-    middle_name: Field(alias="middleName")
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
 
 class CreateUserResponseSchema(BaseModel):
     id: str
     email: EmailStr
-    last_name: Field(alias="lastName")
-    first_name: Field(alias="firstName")
-    middle_name: Field(alias="middleName")
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
 
