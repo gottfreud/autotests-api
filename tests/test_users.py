92bda071aee5570cfb00648cbec6e9eb713f7e6d
Gottfreud <github@sandalow.net>
2026-03-10 22:39:33 +0100

    Фикстуры Pytest

--- tests/test_users.py
+++ tests/test_users.py
@@ -35,5 +35,9 @@ def test_create_user(public_users_client: PublicUsersClient):
 @pytest.mark.regression
 def test_get_user_me(function_user: UserFixture, private_users_client: PrivateUsersClient):
     response = private_users_client.get_user_me_api()
+    response_data = GetUserResponseSchema.model_validate_json(response.text)
+
     assert_status_code(response.status_code, HTTPStatus.OK)
-    assert_get_user_response(GetUserResponseSchema.model_validate_json(response.text), function_user.response)
+    assert_get_user_response(response_data, function_user.response)
+
+    validate_json_schema(response.json(), response_data.model_json_schema())
