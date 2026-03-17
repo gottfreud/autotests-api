e6a0cbeddc4ce7b3162399999eec3f42860b4a6f
Gottfreud <github@sandalow.net>
2026-03-17 13:16:14 +0100

    Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Получение задания

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -3,9 +3,11 @@ from http import HTTPStatus
 import pytest
 
 from clients.exercises.exercises_client import ExerciseClient
-from clients.exercises.exercises_schema import CreateExerciseResponseSchema, CreateExerciseRequestSchema
+from clients.exercises.exercises_schema import CreateExerciseResponseSchema, CreateExerciseRequestSchema, \
+    GetExercisesResponseSchema, GetExercisesSchema
 from fixtures.courses import CourseFixture
-from tools.assertions.exercises import assert_create_exercise_response
+from fixtures.exercises import ExerciseFixture
+from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercises_response, assert_exercise
 from tools.assertions.schema import validate_json_schema
 
 
@@ -19,3 +21,10 @@ class TestExercises:
         assert response.status_code == HTTPStatus.OK
         assert_create_exercise_response(request, response_data)
         validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_get_exercise(self, exercises_client: ExerciseClient, function_exercise: ExerciseFixture):
+        query = GetExercisesSchema(course_id=function_exercise.response.exercise.course_id)
+        response = exercises_client.get_exercises_api(query)
+        response_data = GetExercisesResponseSchema.model_validate_json(response.text)
+        assert response.status_code == HTTPStatus.OK
+        assert_get_exercises_response(response_data, [function_exercise.response])
--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,5 +1,6 @@
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
-from tools.assertions.base import assert_equal
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
+    ExerciseSchema, GetExercisesResponseSchema, GetExercisesSchema
+from tools.assertions.base import assert_equal, assert_length
 
 
 def assert_create_exercise_response(request: CreateExerciseRequestSchema, response: CreateExerciseResponseSchema):
@@ -10,3 +11,19 @@ def assert_create_exercise_response(request: CreateExerciseRequestSchema, respon
     assert_equal(response.exercise.description, request.description, "description")
     assert_equal(response.exercise.estimated_time, request.estimated_time, "estimated_time")
     assert_equal(response.exercise.order_index, request.order_index, "order_index")
+
+def assert_exercise(actual: ExerciseSchema, expected: ExerciseSchema):
+    assert_equal(actual.title, expected.title, "title")
+    assert_equal(actual.course_id, expected.course_id, "course_id")
+    assert_equal(actual.min_score, expected.min_score, "min_score")
+    assert_equal(actual.max_score, expected.max_score, "max_score")
+    assert_equal(actual.description, expected.description, "description")
+    assert_equal(actual.estimated_time, expected.estimated_time, "estimated_time")
+    assert_equal(actual.order_index, expected.order_index, "order_index")
+
+def assert_get_exercises_response(get_exercises_response: GetExercisesResponseSchema, create_exercise_responses: list[CreateExerciseResponseSchema]):
+    assert_length(get_exercises_response.exercises, create_exercise_responses, "exercises")
+    for index, create_exercise_response in enumerate(create_exercise_responses):
+        assert_exercise(
+            get_exercises_response.exercises[index], create_exercise_response.exercise
+        )
