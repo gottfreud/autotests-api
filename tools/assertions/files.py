c931b07000866690858ad2af45461e6328996bfa
Gottfreud <github@sandalow.net>
2026-03-27 15:33:27 +0100

    Добавляем логирование для API автотестов

--- tools/assertions/files.py
+++ tools/assertions/files.py
@@ -5,7 +5,9 @@ from tools.assertions.base import assert_equal
 from tools.assertions.errors import assert_validation_error_response, assert_internal_error_response
 import allure
 from config import settings
+from tools.logger import get_logger
 
+logger = get_logger("FILES_ASSERTIONS")
 
 @allure.step("Check create file response")
 def assert_create_file_response(request: CreateFileRequestSchema, response: CreateFileResponseSchema):
@@ -15,6 +17,7 @@ def assert_create_file_response(request: CreateFileRequestSchema, response: Crea
     :param response: Ответ API с данными файла.
     :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
+    logger.info("Check create file response")
     expected_url = f"{settings.http_client.client_url}static/{request.directory}/{request.filename}"
     assert_equal(str(response.file.url), expected_url, "url")
     assert_equal(response.file.filename, request.filename, "filename")
@@ -22,6 +25,14 @@ def assert_create_file_response(request: CreateFileRequestSchema, response: Crea
 
 @allure.step("Check file")
 def assert_file(actual: FileSchema, expected: FileSchema):
+    """
+        Проверяет, что фактические данные файла соответствуют ожидаемым.
+
+        :param actual: Фактические данные файла.
+        :param expected: Ожидаемые данные файла.
+        :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    logger.info("Check file")
     assert_equal(actual.id, expected.id, "id")
     assert_equal(actual.url, expected.url, "url")
     assert_equal(actual.filename, expected.filename, "filename")
@@ -29,6 +40,14 @@ def assert_file(actual: FileSchema, expected: FileSchema):
 
 @allure.step("Check get file response")
 def assert_get_file_response(get_file_response: GetFileResponseSchema, create_file_response: CreateFileResponseSchema):
+    """
+        Проверяет, что ответ на получение пользователя соответствует ответу на его создание.
+
+        :param get_file_response: Ответ API при запросе данных пользователя.
+        :param create_file_response: Ответ API при создании пользователя.
+        :raises AssertionError: Если данные пользователя не совпадают.
+    """
+    logger.info("Check get file response")
     assert_file(
         actual=get_file_response.file,
         expected=create_file_response.file
@@ -42,6 +61,7 @@ def assert_create_file_with_empty_filename_response(actual: ValidationErrorRespo
         :param actual: Ответ от API с ошибкой валидации, который необходимо проверить.
         :raises AssertionError: Если фактический ответ не соответствует ожидаемому.
     """
+    logger.info("Check create file with empty filename response")
     expected = ValidationErrorResponseSchema(
         details=[
             ValidationErrorSchema(
@@ -63,6 +83,7 @@ def assert_create_file_with_empty_directory_response(actual: ValidationErrorResp
         :param actual: Ответ от API с ошибкой валидации, который необходимо проверить.
         :raises AssertionError: Если фактический ответ не соответствует ожидаемому.
     """
+    logger.info("Check create file with empty directory response")
     expected = ValidationErrorResponseSchema(
         details=[
             ValidationErrorSchema(
@@ -84,6 +105,7 @@ def assert_file_not_found_response(actual: InternalErrorResponseSchema):
         :param actual: Фактический ответ.
         :raises AssertionError: Если фактический ответ не соответствует ошибке "File not found"
     """
+    logger.info("Check file not found response")
     expected = InternalErrorResponseSchema(
         details="File not found"
     )
@@ -96,6 +118,7 @@ def assert_get_file_with_incorrect_file_id_response(actual: ValidationErrorRespo
     :param actual: Ответ от API с ошибкой валидации, которую необходимо проверить.
     :raises AssertionError: Если фактический ответ не соответствует ожидаемому.
     """
+    logger.info("Check get file with incorrect file id response")
     expected = ValidationErrorResponseSchema(
         details=[
             ValidationErrorSchema(
