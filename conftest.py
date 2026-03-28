1be295e0dd55f648a8d3fa3c00b649a3168a5b52
gottfreud <gottfreud@users.noreply.github.com>
2026-03-28 09:39:47 +0000

    deploy: 4c771366aa25383cd8c8568af93fd8cd7aada27e

--- conftest.py
+++
@@ -1,6 +0,0 @@
-pytest_plugins = ["fixtures.authentication",
-                "fixtures.files",
-                  "fixtures.exercises",
-                  "fixtures.courses",
-                  "fixtures.users",
-                  "fixtures.allure"]
